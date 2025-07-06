03559d30fcbab8cecf6d003ba586839ac49b8f97
daonvshu <1286772338@qq.com>
2025-07-06 18:52:08 +0800

    add package script

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.daonvshu.protocol.codec"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -21,4 +21,32 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+sourceSets {
+    main {
+        kotlin {
+            exclude("com.daonvshu.protocol.codec.Main.kt")
+        }
+    }
+}
+
+tasks.register<Jar>("fatJar") {
+    group = "build"
+    description = "生成包含所有依赖的可执行JAR包"
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks.register<Jar>("sourceJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
 }
